3e820789c3df0280578d0d25a279899b17a1883e
bertie <bertiefindlay@gmail.com>
2022-02-28 21:09:26 +0000

    added word service added simple grid view

--- FindlApp/MainWindow.xaml.cs
+++ FindlApp/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FindlApp.Models;
+using FindlApp.Servceis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,38 @@ namespace FindlApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        CheckWordService m_checkWordService;
+        int m_guessNumber;
+
         public MainWindow()
         {
             InitializeComponent();
+            m_checkWordService = new CheckWordService(new Words());
+            m_guessNumber = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            m_checkWordService.ChooseNewWord();
+            m_answer.Text = m_checkWordService.CurrentWord;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string guess = GetWord();
+            m_check.IsChecked = m_checkWordService.Check(guess);
+        }
+
+        private string GetWord()
+        {
+            string word = string.Empty;
+
+            if (m_guessNumber == 0)
+            {
+                word = m_guess1letter1.Text + m_guess1letter2.Text + m_guess1letter3.Text + m_guess1letter4.Text + m_guess1letter5.Text;
+            }
+
+            return word;
         }
     }
 }
---
+++ FindlApp/Servceis/CheckWordService.cs
@@ -0,0 +1,34 @@
+﻿using FindlApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindlApp.Servceis
+{
+    public class CheckWordService
+    {
+        Words m_words;
+        string m_currentWord;
+
+        public CheckWordService(Words words)
+        {
+            m_words = words;
+        }
+
+        public bool Check(string guess)
+        {
+            return m_words.Exists(guess);
+        }
+
+        public void ChooseNewWord()
+        {
+            Random random = new Random();
+            int r = random.Next(m_words.Size);
+            m_currentWord = m_words.Get(r);
+        }
+
+        public string CurrentWord => m_currentWord;
+    }
+}
